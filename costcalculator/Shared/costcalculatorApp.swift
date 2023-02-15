2a78d428fc5ce710a75ffc1103ebebc5291b20dd
remycoopermusic <remyborsboom@hotmail.com>
2023-02-15 20:27:44 +0100

    chore: configured xcode project

---
+++ costcalculator/Shared/costcalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  costcalculatorApp.swift
+//  Shared
+//
+//  Created by Remy Borsboom on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct costcalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
